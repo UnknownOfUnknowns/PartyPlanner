61151633240dae0a633d4b5a2f34d866798a4706
Unknown Unknown <hans@bilstrupandersen.dk>
2022-10-30 19:47:20 +0100

    font added

--- app/src/main/java/com/example/partyplanner/ui/theme/Type.kt
+++ app/src/main/java/com/example/partyplanner/ui/theme/Type.kt
@@ -2,14 +2,20 @@ package com.example.partyplanner.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.partyplanner.R
 
+val Roboto = FontFamily(
+    Font(resId = R.font.roboto_regular, weight = FontWeight.W100, style = FontStyle.Normal)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyMedium = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Roboto,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
@@ -26,3 +32,4 @@ val Typography = Typography(
     )
     */
 )
+
