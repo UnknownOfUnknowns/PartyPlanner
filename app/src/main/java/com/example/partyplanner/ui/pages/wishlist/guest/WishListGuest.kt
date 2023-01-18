bb0972f555462a8b6820999a937c14c37f9f5b2e
Unknown Unknown <hans@bilstrupandersen.dk>
2023-01-18 09:44:58 +0100

    sharebuttons removed

--- app/src/main/java/com/example/partyplanner/ui/pages/wishlist/guest/WishListGuest.kt
+++ app/src/main/java/com/example/partyplanner/ui/pages/wishlist/guest/WishListGuest.kt
@@ -1,18 +1,18 @@
 package com.example.partyplanner.ui.elements
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.partyplanner.data.wish.WishServiceImpl
 import com.example.partyplanner.ui.pages.wishlist.WishListViewModel
 import com.example.partyplanner.ui.pages.wishlist.WishUiState
 import com.example.partyplanner.ui.theme.Background
-import com.google.firebase.firestore.FirebaseFirestore
 
 @Composable
 fun WishListGuestPage(viewModel: WishListViewModel, navigateToProduct: (WishUiState) -> Unit) {
@@ -23,12 +23,10 @@ fun WishListGuestPage(viewModel: WishListViewModel, navigateToProduct: (WishUiSt
             .background(Background)
     ) {
         Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
-            NameCardWishList(name = uiState.value.wishListName, modifier = Modifier.size(width = 350.dp, height = 120.dp ))
-            Spacer(modifier = Modifier.height(17.dp))
-            WishList(uiState.value.wishes, showTopBar = true, onImageClick = { navigateToProduct(it) })
+            WishList(uiState.value.wishes,
+                showTopBar = true, onImageClick = { navigateToProduct(it) },
+                modifier = Modifier.fillMaxSize().padding(10.dp))
         }
-        ShareFAB(modifier = Modifier
-            .align(Alignment.BottomEnd), onClick = {})
     }
 }
 
